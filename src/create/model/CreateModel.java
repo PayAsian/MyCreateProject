0bccc44e8aeb766eb96fa253f662092e45225274
PayAsian <PayAsian@yahoo.com>
2017-02-03 13:11:53 -0700

    after peer review
    just comments from people

--- src/create/model/CreateModel.java
+++ src/create/model/CreateModel.java
@@ -9,16 +9,23 @@ public class CreateModel
 
 	public boolean Buildlist(boolean BuildList)
 	{
-	// The topic's should be changed to something relevant to the project
+	// *note to self* The topic's should be changed to something relevant to the project
 	topicList.add("Potato");
 	topicList.add("Chinese Food");
 	topicList.add("Indian Food");
 	topicList.add("Mexican Food");
 	topicList.add("Cheese Burger");
 	topicList.add("7");
-	
+
 	topicList.get(3);
 	
 	return BuildList;
 	}
 }
+//YEAH NICE WORK BOI YOU ARE SO GOOD AND YOU ALWAYS MAKE YOUR PARENTS PROUD. 
+//JUST BECAUSE YOU'RE A LITTLE ASIAN DOENS'T MEAN YOU CANT BE STOPED YOU CAN DO ANYTHING AND I THINK YOUR CODE IS SO GOOD
+//KEEP DOING GOOD THINGS BOI YOU ARE ONE IN A MILLION KEEP BEING THE MAN THAT HAS A PLAY *HUG xDDD* KEEP YOUR HEAD UP AND
+//NEVER STOP BEING DAT BOI!
+//RAWRRRRRRR
+
+//OwO whats this??? 
